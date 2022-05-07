4dfca3646b9ff06d3fa467a9593a1163b1e533fd
manjeetdhayal <manjeetdhayal786@gmail.com>
2022-05-07 15:54:31 +0530

    React Native: Refresh on Pull functionality

--- screens/Home.js
+++ screens/Home.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, RefreshControl } from 'react-native';
 import PalettePreview from '../components/PalettePreview';
 import { useState, useCallback, useEffect } from 'react';
 //we make API call to fetch color
 
 const Home = ({ navigation }) => {
   const [colors, setColors] = useState([]);
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
   //we make a async call to fetch the colors
 
   const fetchColors = useCallback(async () => {
+    // console.log('I am in fetch colors'); //working
     await fetch('https://color-palette-api.kadikraman.now.sh/palettes')
       .then((res) => res.json())
       .then((res) => {
         setColors(res);
-        console.warn(res);
       });
   });
 
   useEffect(() => {
     fetchColors();
+  }, []);
+
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    // console.log('I am in refresh'); //working
+    await fetchColors();
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 500);
   }, []);
 
   return (
@@ -35,6 +46,14 @@ const Home = ({ navigation }) => {
           />
         )}
         keyExtractor={(paletteName) => paletteName.paletteName}
+        // refreshControl={
+        // <RefreshControl refreshing={true} onRefresh={() => {}} /> //note we can use native refresher as well that is built in in FlatList
+        //}  //we can make custom refresher as well using this
+
+        refreshing={isRefreshing}
+        onRefresh={() => {
+          handleRefresh();
+        }}
       />
     </View>
   );
